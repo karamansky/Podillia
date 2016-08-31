80212cf7db1fc1c5bb3f0460fde3fd99f2c7d173
karamansky <karamansky@gmail.com>
2016-08-31 22:01:48 +0300

    index - 50%

--- js/common.js
+++ js/common.js
@@ -4,10 +4,27 @@ $(function(){
 	$(document).ready(function() {
 
 		//owl carousel init
-		$(".owl-carousel").owlCarousel();
-
-		//
-		
+		$(".owl-carousel").owlCarousel({
+			items 					: 1,
+			// autoHeight			: true,
+			// autoWidth 			: true,
+			itemClass				: ".field__item",
+			responsive			: false,
+			nav: true
+		});$(".seed-carousel").owlCarousel({
+			items 					: 4,
+			itemClass				: ".seed__item",
+			responsive			: false,
+			navigation			: true,
+			navigationText	: ["",""]
+		});
+		var owl = $(".owl-carousel").data('owlCarousel');
+		$(".prev").click(function(){
+			owl.prev();
+		});
+		$(".next").click(function(){
+			owl.next();
+		});
 
 
 
